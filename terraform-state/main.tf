8fddba548f30cdfbfd8337a63340a91af0aca3f1
Keunbae Park <pkboom@users.noreply.github.com>
2022-11-12 22:02:09 -0500

    wip

--- terraform-state/main.tf
+++ terraform-state/main.tf
@@ -15,7 +15,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-keunbae"
+  bucket        = "cloudcasts-terraform-keunbae2"
+  force_destroy = true # delete all objects in the bucket even when not empty
 
   # Prevent accidental deletion of this S3 bucket
   lifecycle {
@@ -43,7 +44,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks-keunbae"
+  name         = "cloudcasts-terraform-keunbae"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
