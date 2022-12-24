b45e35ad6a6fad6c6fff2c1f13b484ba88f67cd3
Keunbae Park <pkboom@users.noreply.github.com>
2022-12-24 16:49:06 -0500

    wip

--- digitalocean-experiment/staging/main.tf
+++ digitalocean-experiment/staging/main.tf
@@ -14,12 +14,18 @@ provider "digitalocean" {
 variable "api_token" {}
 
 data "digitalocean_image" "this" {
-  name = "experiment-web-2022_12_23 09:52"
+  name = "experiment-web"
 }
 
 resource "digitalocean_droplet" "web" {
   image  = data.digitalocean_image.this.id
-  name   = "${var.infra_name}-${var.infra_env}"
+  name   = "${var.infra_name}-${var.infra_env}-web"
   region = "tor1"
   size   = "s-1vcpu-1gb"
+  tags = {
+    Project     = "experiment.com"
+    Environment = var.infra_env
+    Type        = "web"
+    ManagedBy   = "terraform"
+  }
 }
